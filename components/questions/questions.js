a041a97d636bc2ae063b663dd85ecf7b36b3a850
Victor Higoy <higoyvictor143@gmail.com>
2022-09-03 23:21:15 +0800

    optimize the question section and adjust the max-wdith of the articledetails paragraph

--- components/questions/questions.js
+++ components/questions/questions.js
@@ -1,102 +1,76 @@
+import { useState } from "react";
 import Subtitles from "../ui/subTitle";
 
 function questions() {
+  const items = [
+    {
+      title: "What is BMI (Body Mass Index) ?",
+      headingNumber: "flush-headingOne",
+      collapseNumber: "flush-collapseOne",
+      description:
+        "Body mass index (BMI) is a person's weight in kilograms divided by the square of height in meters. BMI is an inexpensive and easy screening method for weight category—underweight, healthy weight, overweight, and obesity. BMI does not measure body fat directly, but BMI is moderately correlated with more direct measures of body fat. Furthermore, BMI appears to be as strongly correlated with various metabolic and disease outcome as are these more direct measures of body fatness. - Centers for Disease Control and Prevention",
+    },
+    {
+      title: "What is BMR (Basal Metabolic Rate) ?",
+      headingNumber: "flush-headingTwo",
+      collapseNumber: "flush-collapseTwo",
+      description:
+        " Basal metabolic rate (BMR) is the total number of calories your body needs to perform essential, life-sustaining functions. Knowing your basal metabolic rate helps you determine the estimated baseline amount of calories your body requires to function. This works as a starting point to determine how many calories you may want to consume based on your goals. Below, learn more about basal metabolic rate and how it applies to you. - Anisha Shah, MD",
+    },
+    {
+      title: "What is 7 Day Calories Cycle (Zig-Zag Diet) ?",
+      headingNumber: "headingThree",
+      collapseNumber: "flush-collapseThree",
+      description:
+        "The Zig Zag Diet, also known as calorie shifting, involves staggering a low-calorie diet with high-calorie days. The belief is that if you can keep your body guessing, you can avoid homeostasis. By confusing your body, the hope is that your metabolism will shift into high gear burning calories more effectively. Zigzag Calorie Diet by Frankie Smith",
+    },
+  ];
+
+  items.map((item, i) => {
+    console.log(item.title);
+  });
+
   return (
     <section className="p-0 pb-5 p-sm-5 bg-white text-dark" id="questions">
       <div className="container">
         <Subtitles subtitle="COMMON QUESTIONS" />
-        <div className="accordion accordion-flush" id="accordionFlushExample">
+        <div
+          className="accordion accordion-flush m-auto"
+          style={{ maxWidth: "1000px" }}
+          id="accordionFlushExample"
+        >
           <div className="accordion-item">
-            <h2 className="accordion-header" id="flush-headingTwo">
-              <button
-                className="accordion-button collapsed fw-bold"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseTwo"
-                aria-expanded="false"
-                aria-controls="flush-collapseTwo"
-              >
-                What is BMI (Body Mass Index) ?
-              </button>
-            </h2>
-            <div
-              id="flush-collapseTwo"
-              className="accordion-collapse collapse"
-              aria-labelledby="flush-headingTwo"
-              data-bs-parent="#accordionFlushExample"
-            >
-              <div className="accordion-body" style={{ textAlign: "justify" }}>
-                &quot;Body mass index (BMI) is a person&apos;s weight in
-                kilograms divided by the square of height in meters. BMI is an
-                inexpensive and easy screening method for weight
-                category—underweight, healthy weight, overweight, and obesity.
-                BMI does not measure body fat directly, but BMI is moderately
-                correlated with more direct measures of body fat. Furthermore,
-                BMI appears to be as strongly correlated with various metabolic
-                and disease outcome as are these more direct measures of body
-                fatness.&quot; - Centers for Disease Control and Prevention
-              </div>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="flush-headingOne">
-              <button
-                className="accordion-button collapsed fw-bold"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseOne"
-                aria-expanded="false"
-                aria-controls="flush-collapseOne"
-              >
-                What is BMR (Basal Metabolic Rate) ?
-              </button>
-            </h2>
-            <div
-              id="flush-collapseOne"
-              className="accordion-collapse collapse"
-              aria-labelledby="flush-headingOne"
-              data-bs-parent="#accordionFlushExample"
-            >
-              <div className="accordion-body" style={{ textAlign: "justify" }}>
-                &quot;Basal metabolic rate (BMR) is the total number of calories
-                your body needs to perform essential, life-sustaining functions.
-                Knowing your basal metabolic rate helps you determine the
-                estimated baseline amount of calories your body requires to
-                function. This works as a starting point to determine how many
-                calories you may want to consume based on your goals. Below,
-                learn more about basal metabolic rate and how it applies to
-                you.&quot; - Anisha Shah, MD
-              </div>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="flush-headingThree">
-              <button
-                className="accordion-button collapsed fw-bold"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseThree"
-                aria-expanded="false"
-                aria-controls="flush-collapseThree"
-              >
-                What is 7 Day Calories Cycle (Zig-Zag Diet) ?
-              </button>
-            </h2>
-            <div
-              id="flush-collapseThree"
-              className="accordion-collapse collapse"
-              aria-labelledby="flush-headingThree"
-              data-bs-parent="#accordionFlushExample"
-            >
-              <div className="accordion-body" style={{ textAlign: "justify" }}>
-                &quot;The Zig Zag Diet, also known as calorie shifting, involves
-                staggering a low-calorie diet with high-calorie days. The belief
-                is that if you can keep your body guessing, you can avoid
-                homeostasis. By confusing your body, the hope is that your
-                metabolism will shift into high gear burning calories more
-                effectively.&quot; Zigzag Calorie Diet by Frankie Smith
-              </div>
-            </div>
+            {items.map((item) => {
+              return (
+                <>
+                  <h2 className="accordion-header" id={item.headingNumber}>
+                    <button
+                      className="accordion-button collapsed fw-bold"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#${item.collapseNumber}`}
+                      aria-expanded="false"
+                      aria-controls={item.collapseNumber}
+                    >
+                      {item.title}
+                    </button>
+                  </h2>
+                  <div
+                    id={item.collapseNumber}
+                    className="accordion-collapse collapse"
+                    aria-labelledby={item.headingNumber}
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div
+                      className="accordion-body"
+                      style={{ textAlign: "justify" }}
+                    >
+                      {item.description}
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
